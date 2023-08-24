b54e9153a3b7998b075661828e817651120fa7d6
rodrigonerisalves <programadorrodrigoneris@gmail.com>
2023-08-24 07:48:48 -0400

    Desafio Mong e SQL

---
+++ Integrando Python com SQLite e MongoDB/integration_with_SQL/sqlAlchemyCoreApplication.py
@@ -0,0 +1,44 @@
+from sqlalchemy import create_engine, Column, Integer, String, Table, MetaData, ForeignKey, text
+
+engine = create_engine('sqlite:///:memory:')
+
+metadata_obj = MetaData()
+user = Table(
+    'user',
+    metadata_obj,
+    Column('user_id', Integer, primary_key=True),
+    Column('user_name', String(40), nullable=False),
+    Column('email_address', String(60)),
+    Column('nickname', String(50), nullable=False)
+)
+sql_insert = text("insert into user values(1, 'juliana', 'email@email.com', 'ju')")
+with engine.connect() as conn:
+    conn.execute(sql_insert)
+
+user_prefs = Table(
+    'userprefs', metadata_obj,
+    Column('pref_id', Integer, primary_key=True),
+    Column('user_id', Integer, ForeignKey('user.user_id'), nullable=False),
+    Column('pref_name', String(40), nullable=False),
+    Column('pref_value', String(100)),
+)
+
+metadata_obj.create_all(engine)
+
+metadata_db_obj = MetaData()
+financial_info = Table(
+    'financial_info',
+    metadata_db_obj,
+    Column('id', Integer, primary_key=True),
+    Column('value', String(100), nullable=False)
+)
+
+for table in metadata_obj.sorted_tables:
+    print(table)
+
+print('Executando statement sql')
+sql = text('select * from user')
+with engine.connect() as conn:
+    result = conn.execute(sql)
+    for num in result:
+        print(num)
